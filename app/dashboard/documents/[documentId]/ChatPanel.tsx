b91d7966f28af0ec080d93491541bdd98a2b95c1
PanjvaniGaurav4 <panjvanigaurav4@gmail.com>
2024-08-09 21:31:45 +0530

    updating metadata and favicon

--- app/dashboard/documents/[documentId]/ChatPanel.tsx
+++ app/dashboard/documents/[documentId]/ChatPanel.tsx
@@ -11,30 +11,32 @@ const ChatPanel = ({ documentId }: { documentId: Id<"documents"> }) => {
 
   return (
     <div className="w-full flex flex-col gap-2 p-4 sm:p-4 max-sm:h-[calc(100vh-320px)] h-[calc(100vh-250px)] bg-slate-200 dark:bg-gray-900 rounded-lg mb-2 relative overflow-hidden">
-      <div className="overflow-auto flex-1 space-y-4 pb-20"> 
+      <div className="flex-1 overflow-auto pb-20">
         <div className="dark:bg-slate-950 bg-slate-300 rounded p-3 dark:text-gray-300 max-sm:text-lg">
           AI: Ask any question about the document below
         </div>
-        {chats?.map((chat, index) => (
-          <div
-            key={index}
-            className={cn(
-              "rounded p-3",
-              chat.isHuman
-                ? "dark:bg-slate-800 bg-gray-300 max-sm:text-lg ml-auto max-w-[90%] sm:max-w-[80%]"
-                : "dark:bg-gray-950 bg-slate-300 max-sm:text-lg mr-auto max-w-[90%] sm:max-w-[80%]"
-            )}
-          >
-            <div className="font-semibold max-sm:text-lg mb-1">
-              {chat.isHuman ? "You" : "AI"}:
+        <div className="space-y-4">
+          {chats?.map((chat, index) => (
+            <div
+              key={index}
+              className={cn(
+                "rounded p-3",
+                chat.isHuman
+                  ? "dark:bg-slate-800 bg-gray-300 max-sm:text-lg ml-auto max-w-[90%] sm:max-w-[80%]"
+                  : "dark:bg-gray-950 bg-slate-300 max-sm:text-lg mr-auto max-w-[90%] sm:max-w-[80%]"
+              )}
+            >
+              <div className="font-semibold max-sm:text-lg mb-1">
+                {chat.isHuman ? "You" : "AI"}:
+              </div>
+              {chat.isHuman ? (
+                <div className="max-sm:text-lg">{chat.text}</div>
+              ) : (
+                <FormattedAIResponse text={chat.text} />
+              )}
             </div>
-            {chat.isHuman ? (
-              <div className="max-sm:text-lg">{chat.text}</div>
-            ) : (
-              <FormattedAIResponse text={chat.text} />
-            )}
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
       <div className="absolute bottom-0 left-0 right-0 bg-slate-200 dark:bg-gray-900 p-2">
         <QuestionForm documentId={documentId} />
